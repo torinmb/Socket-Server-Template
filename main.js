e916a5e762d3f71a968f27aa0a1c3f169d6c05fa
Torin Blankensmith <torin.blankensmith@gmail.com>
2023-09-10 13:43:20 -0400

    multi client

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 const http = require("http");
 const express = require("express");
 const app = express();
-
-app.use(express.static("public"));
-// require("dotenv").config();
-
-const serverPort = process.env.PORT || 3000;
-const server = http.createServer(app);
 const WebSocket = require("ws");
 
-let keepAliveId;
+// Constants
+const ACTIVE_LIMIT = 20;
+
+// Storage for clients and queue
+let mainClients = [];
+let queuedClients = [];
+let touchDesignerClient = null;
+
+// Server setup
+const serverPort = process.env.PORT || 3000;
+const server = http.createServer(app);
 
 const wss =
   process.env.NODE_ENV === "production"
@@ -21,63 +25,89 @@ console.log(`Server started on port ${serverPort} in stage ${process.env.NODE_EN
 
 wss.on("connection", function (ws, req) {
   console.log("Connection Opened");
-  console.log("Client size: ", wss.clients.size);
-
-  if (wss.clients.size === 1) {
-    console.log("first connection. starting keepalive");
-    keepServerAlive();
-  }
 
   ws.on("message", (data) => {
     let stringifiedData = data.toString();
+
     if (stringifiedData === 'pong') {
       console.log('keepAlive');
       return;
     }
-    broadcast(ws, stringifiedData, false);
+
+    if (stringifiedData === 'TOUCHDESIGNER_ID') {
+      touchDesignerClient = ws;
+      console.log("TouchDesigner client connected!");
+      return;
+    }
+
+    if (touchDesignerClient) {
+      touchDesignerClient.send(stringifiedData);
+    }
   });
+
+  // Manage new connections
+  if (mainClients.length < ACTIVE_LIMIT) {
+    mainClients.push(ws);
+    ws.send(JSON.stringify({ type: 'status', status: 'connected' }));
+  } else {
+    queuedClients.push(ws);
+    ws.send(JSON.stringify({ type: 'status', status: 'queued' }));
+  }
 
   ws.on("close", (data) => {
     console.log("closing connection");
 
-    if (wss.clients.size === 0) {
-      console.log("last client disconnected, stopping keepAlive interval");
-      clearInterval(keepAliveId);
+    // Check if TouchDesigner client is disconnecting
+    if (ws === touchDesignerClient) {
+        console.log("TouchDesigner client disconnected!");
+        touchDesignerClient = null;
+        broadcast(null, JSON.stringify({ type: 'status', status: 'touchDesignerOffline' }), true);
+    }
+
+    // Handle other clients
+    mainClients = mainClients.filter(client => client !== ws);
+    queuedClients = queuedClients.filter(client => client !== ws);
+
+    // Promote queued clients
+    if (mainClients.length < ACTIVE_LIMIT && queuedClients.length > 0) {
+      const promotedClient = queuedClients.shift();
+      mainClients.push(promotedClient);
+      promotedClient.send(JSON.stringify({ type: 'status', status: 'promoted' }));
     }
   });
 });
 
 // Implement broadcast function because of ws doesn't have it
 const broadcast = (ws, message, includeSelf) => {
-  if (includeSelf) {
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  } else {
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  }
-};
-
-/**
- * Sends a ping message to all connected clients every 50 seconds
- */
- const keepServerAlive = () => {
-  keepAliveId = setInterval(() => {
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send('ping');
-      }
-    });
-  }, 50000);
-};
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+    if (includeSelf) {
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(message);
+        }
+      });
+    } else {
+      wss.clients.forEach((client) => {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(message);
+        }
+      });
+    }
+  };
+  
+  /**
+   * Sends a ping message to all connected clients every 50 seconds
+   */
+   const keepServerAlive = () => {
+    keepAliveId = setInterval(() => {
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send('ping');
+        }
+      });
+    }, 50000);
+  };
+  
+  
+  app.get('/', (req, res) => {
+      res.send('Hello World!');
+  });
